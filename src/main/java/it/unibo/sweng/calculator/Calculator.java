40814a7c082f3f32d5970528ac27146e66e5868d
Davide Rossi <daviderossi@unibo.it>
2026-04-17 14:59:36 +0200

    First few tests

--- src/main/java/it/unibo/sweng/calculator/Calculator.java
+++ src/main/java/it/unibo/sweng/calculator/Calculator.java
@@ -2,6 +2,18 @@ package it.unibo.sweng.calculator;
 
 public class Calculator {
     public int compute(String expression) {
-        return 0;
+        if (expression == null || "".equals(expression)) {
+            throw new IllegalArgumentException("expression cannot be null");
+        }
+        if (expression.contains("+")) {
+            int operatorPosition = expression.indexOf("+");
+            int a = Integer.parseInt(expression.substring(0, operatorPosition));
+            int b = Integer.parseInt(expression.substring(operatorPosition+1));
+            return a + b;
+        }
+        if (expression == null || "".equals(expression) || expression.length() > 3) {
+            throw new IllegalArgumentException("expression cannot be null");
+        }
+        return Integer.parseInt(expression);
     }
 }
--- src/test/java/it/unibo/sweng/calculator/CalculatorTest.java
+++ src/test/java/it/unibo/sweng/calculator/CalculatorTest.java
@@ -5,4 +5,56 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+    // invalid tokens -> IllegalArgumentException
+    // division by zero -> ArithmeticException
+    // division
+    // product
+    // expression with all operators
+    // expression with inverted precedence
+
+    // null -> IllegalArgumentException
+    @Test
+    void computeWithNullParameterShouldRaiseIllegalArgumentException() {
+        Calculator calculator = new Calculator();
+        assertThrows(IllegalArgumentException.class,
+            () -> calculator.compute(null));
+    }
+
+    // empty string -> IllegalArgumentException
+    @Test
+    void computeWithEmptyStringParameterShouldRaiseIllegalArgumentException() {
+        Calculator calculator = new Calculator();
+        assertThrows(IllegalArgumentException.class,
+            () -> calculator.compute(""));
+    }
+
+    // just a number
+    @Test
+    void computeWithSingleNumberShouldReturnItsValue() {
+        Calculator calculator = new Calculator();
+        assertEquals(123, calculator.compute("123"));
+    }
+ 
+    // numbers must not be longer than 3 digits
+    @Test
+    void computeWithSingleLongNumberShouldRaiseIllegalArgumentException() {
+        Calculator calculator = new Calculator();
+        assertThrows(IllegalArgumentException.class,
+            () -> calculator.compute("1234"));
+    }
+    
+    // sum
+    @Test
+    void computeWithSingleSumShouldReturnItsValue() {
+        Calculator calculator = new Calculator();
+        assertEquals(235, calculator.compute("1+234"));
+    }
+
+    // subtraction
+    @Test
+    void computeWithSingleSubtractionShouldReturnItsValue() {
+        Calculator calculator = new Calculator();
+        assertEquals(965, calculator.compute("971-6"));
+    }
+
 }
